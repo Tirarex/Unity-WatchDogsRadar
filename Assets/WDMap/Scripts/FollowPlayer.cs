fb7c9b8721295c16a7bb63195a9ddc7bb005dc16
Tirarex <tirarex98@gmail.com>
2020-05-16 18:39:48 +0400

    1st upload

---
+++ Assets/WDMap/Scripts/FollowPlayer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    public Transform PlayerPivot;
+    void Start()
+    {
+        
+    }
+
+    Vector3 Rot;
+    void Update()
+    {
+        this.transform.position = PlayerPivot.position;
+        Rot = new Vector3(0, PlayerPivot.eulerAngles.y,0);
+        this.transform.eulerAngles = Rot;
+    }
+}
